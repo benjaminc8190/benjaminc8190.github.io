eedef9dab98efe8ae9bb50923290e2376474b094
benjaminc8190 <114585884+benjaminc8190@users.noreply.github.com>
2022-09-28 08:40:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Benjamin Chau"; // update the part BETWEEN the "quotes"
+let userName = "benjaminc8190"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
